9c311ff1324d4f991f6861e85540445aa6d73aaa
DongLieu <duongduydongtq@gmail.com>
2023-03-15 23:34:25 +0700

    m

--- depinject/check_type.go
+++ depinject/check_type.go
@@ -63,7 +63,7 @@ func isExportedType(typ reflect.Type) error {
 		return isExportedType(typ.Elem())
 
 	default:
-		// all the remaining types are builtin, non-composite types (like integers), so they are fine to use
+		// all the remaining types are builtin, non-composite types (like integers), so they are fine to useasdda
 		return nil
 	}
 }
